f5d508bf5b56747c8b0f2464eecd16a633d7fd43
whj <whj@dbmr-913.campus.unibe.ch>
2024-06-04 16:50:48 +0200

    completed task

--- task_1.py
+++ task_1.py
@@ -16,9 +16,24 @@ def divide(x, y):
         return "Cannot divide by zero."
 
 
-
-
 # Task 1: Implement a function for Exponentiation operation, similar to the existing functions above.
+def exponentiation(x, y):
+    return x**y
+
 # Task 2: Create an interactive user input for the calculator, allowing the user to pick an operation and input numbers.
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+operator = input("Enter a operator (+,-,*,/,**): ")
+if operator == "+":
+    result = add(x, y)
+elif operator == "-":
+    result = subtract(x, y)
+elif operator == "*":
+    result = multiply(x, y)
+elif operator == "/":
+    result = divide(x, y)
+else:
+    result = exponentiation(x, y)
+
 # Task 3: Add a new file to the repo with other code of your choice
 
